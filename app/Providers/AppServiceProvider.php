d8ceb7f8ccef0a9ec21a92a9ae01b5d40b31571d
kenbabyface <tiamiyugabriel1@gmail.com>
2026-03-24 11:04:56 +0100

    Fix mixed content issue by forcing HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,20 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-
     public function register(): void
     {
     
     }
 
-
     public function boot(): void
     {
-         if ($this->app->environment('production')) {
-        // \Illuminate\Support\Facades\URL::forceScheme('https');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
