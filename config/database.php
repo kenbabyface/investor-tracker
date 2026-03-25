f61adf1436c185111f97251b5522d9c7dff625b4
kenbabyface <tiamiyugabriel1@gmail.com>
2026-03-25 11:59:58 +0100

    Fix SSL options for local development

--- config/database.php
+++ config/database.php
@@ -58,10 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => [
+            'options' => env('MYSQL_ATTR_SSL_CA') ? [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ],
+            ] : [],
         ],
 
         'mariadb' => [
